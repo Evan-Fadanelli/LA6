d03166fc523b1472a78096c53008d46aeae8cd14
schuy <schuy@10.80.140.97>
2018-11-20 14:55:01 -0500

    some more

--- LA6/src/edu/wmich/cs1120/efadanelli/LA6/Decoder.java
+++ LA6/src/edu/wmich/cs1120/efadanelli/LA6/Decoder.java
@@ -8,16 +8,22 @@ public class Decoder implements IDecoder {
 	char[] spaget= new char[65000];
 	int index = 0;
 	int indexPart2 = 0;
-	char[indexPart2] = spaget[index];
+	
 	do {
 		spaget[indexPart2] = petter[index];
 		int fire = petter[index+2].charValue();
 		indexPart2++;
 		index = index + fire;
-		
-		
-		
+			
 	}while(fire != -1)
-	
+	for(int i = 0; i<spaget.lengths; i++)
+	{
+		System.out.print(spaget[i]);
+		if(spaget[i-1].equals("/") && spaget[i].equals("n"))
+		{
+			System.out.println();
+		}
+	}
 	}	
+	
 }
